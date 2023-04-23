e460c7e10f9f8f29038b061c4fdec8eef450dd73
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-23 12:08:47 +0800

    Pseudocode

---
+++ Square-Even-Cube-Odd-Numbers.py
@@ -0,0 +1,14 @@
+# Kristine Joy Barrina  #BSCPE 1-5  # April 23, 2023
+# Creating a python program that will create two different files then square all even numbers and cube all odd numbers.
+
+# Pseudocode
+
+# Store all integers in a variable list
+# Use for loop with range of all integers length
+# If integer is even, square it
+# Open a file named double.txt
+# Append the squared value of even integers to double.txt
+# If integer is odd, cube it
+# Open a file named triple.txt
+# Append the cubic value of odd integers to triple.txt
+# Design the output using tkinter
